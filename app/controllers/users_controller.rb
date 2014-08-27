ea57b73c756a9e16553813d7993398372d662aec
Markv12 <swsteffes@gmail.com>
2014-08-26 23:10:16 -0500

    Added authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,25 @@ class UsersController < ApplicationController
     @user = current_user
   end
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Registration Successful"
+      redirect_back_or_default root_path
+    else
+      render :action => :new
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :name, :password, :password_confirmation)
+  end
+
+
   private
   def login_required
     unless current_user
