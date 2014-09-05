d29c4340283b9221cf83dd0f8ffeb7376dc2adcd
markv12 <scott@Scotts-MacBook-Pro.local>
2014-09-05 13:49:54 -0500

    fixed route name issue

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'front_page#index'
 
   get '/me', to: 'users#show'
-  get '/users/new', to: 'users#new'
+  get '/users/new', to: 'users#new', as: 'new_user'
   post '/users', to: 'users#create'
   get '/entries/:id', to: 'entries#show', as: 'entry'
   patch '/entries/:id', to: 'entries#update', as: 'entry_update'
