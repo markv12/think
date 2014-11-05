f41e876ff7a2a63e99b5553aa2cdc30d07d23eaf
Markv12 <swsteffes@gmail.com>
2014-11-05 09:13:35 -0600

    beautified activeadmin view

--- app/admin/category.rb
+++ app/admin/category.rb
@@ -1,3 +1,11 @@
 ActiveAdmin.register Category do
   permit_params :name, :abbreviation, :description
+  form do |f|
+    f.inputs "Category Information" do
+      f.input :name, :input_html => {:rows => 1, :cols => 10}
+      f.input :abbreviation, :input_html => {:rows => 1, :cols => 10}
+      f.input :description
+    end
+    f.actions
+  end
 end
