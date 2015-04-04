f45e9fe7987b2b607223b6e8580ec23570ae216a
Markv12 <swsteffes@gmail.com>
2015-04-04 15:04:59 -0500

    added validation for habit creation

--- app/admin/habit.rb
+++ app/admin/habit.rb
@@ -9,6 +9,7 @@ ActiveAdmin.register Habit do
 
   form do |f|
     f.inputs "Habit Information" do
+      f.semantic_errors *f.object.errors.keys
       f.input :name, :input_html => {:rows => 1, :cols => 10}
       f.input :description
       f.input :categories,:as => :select, :input_html => {:multiple => true}
--- app/models/habit.rb
+++ app/models/habit.rb
@@ -1,3 +1,14 @@
 class Habit < ActiveRecord::Base
   has_and_belongs_to_many :categories
+
+  validates :name, :description, presence: true
+  validate :has_category
+
+  private
+
+  def has_category
+    if categories.count == 0
+      errors[:base] <<  "A habit must have at least one category"
+    end
+  end
 end
