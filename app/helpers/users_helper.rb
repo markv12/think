5776aee44258ebd3ff35ad5301d3850d5bf7f470
Scott Steffes <swsteffes@gmail.com>
2018-03-21 18:10:31 +0900

    Prettify things a bit

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,4 +2,12 @@ module UsersHelper
   def lerp(a, b, f)
     return a + f * (b - a);
   end
+
+  def min(first, second)
+    if(first <= second)
+      first
+    else
+      second
+    end
+  end
 end
