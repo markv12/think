989495fecbe19f3b17a82db53f9114d2606f41fc
Scott Steffes <swsteffes@gmail.com>
2025-03-02 12:04:06 -0600

    Update Versions
    Update Versions
    
    Update Versions
    
    change to dark mode
    
    input text color
    
    Update Versions
    
    Update to Rails 7
    
    use yjit
    
    Update Versions
    
    Update Versions
    
    Update Versions
    
    Update Versions
    
    Update Versions
    
    Update Versions
    
    Update Versions
    
    Update Versions
    
    Update Versions
    
    Update Versions
    
    Update Versions
    
    Update Versions

--- db/schema.rb
+++ db/schema.rb
@@ -2,16 +2,15 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181219061228) do
-
+ActiveRecord::Schema[7.2].define(version: 2023_11_10_023407) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,8 +23,8 @@ ActiveRecord::Schema.define(version: 20181219061228) do
     t.text "name"
     t.text "abbreviation"
     t.text "description"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", precision: nil
+    t.datetime "updated_at", precision: nil
   end
 
   create_table "categories_habits", id: false, force: :cascade do |t|
@@ -39,8 +38,8 @@ ActiveRecord::Schema.define(version: 20181219061228) do
   end
 
   create_table "entries", id: :serial, force: :cascade do |t|
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", precision: nil
+    t.datetime "updated_at", precision: nil
     t.text "text"
     t.integer "user_id"
     t.integer "wordcount", default: 0
@@ -49,8 +48,8 @@ ActiveRecord::Schema.define(version: 20181219061228) do
   create_table "user_sessions", id: :serial, force: :cascade do |t|
     t.string "session_id", limit: 255, null: false
     t.text "data"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", precision: nil
+    t.datetime "updated_at", precision: nil
     t.index ["session_id"], name: "index_user_sessions_on_session_id"
     t.index ["updated_at"], name: "index_user_sessions_on_updated_at"
   end
@@ -65,15 +64,14 @@ ActiveRecord::Schema.define(version: 20181219061228) do
     t.string "perishable_token", limit: 255, null: false
     t.integer "login_count", default: 0, null: false
     t.integer "failed_login_count", default: 0, null: false
-    t.datetime "last_request_at"
-    t.datetime "current_login_at"
-    t.datetime "last_login_at"
+    t.datetime "last_request_at", precision: nil
+    t.datetime "current_login_at", precision: nil
+    t.datetime "last_login_at", precision: nil
     t.string "current_login_ip", limit: 255
     t.string "last_login_ip", limit: 255
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", precision: nil
+    t.datetime "updated_at", precision: nil
     t.integer "daily_assessment_id"
     t.boolean "admin", default: false
   end
-
 end
